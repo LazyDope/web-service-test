9a4979e402690b63940cd0f9c7251d7239f570dc
LazyDope <mydj555@gmail.com>
2025-02-24 17:18:51 -0500

    add listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing, Router};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new().route("/", routing::get(root_get));
+    let listener = TcpListener::bind("0.0.0.0:7040").await?;
+    println!("Listening on {}", listener.local_addr()?);
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+async fn root_get() -> String {
+    String::from("Hello world!")
 }
